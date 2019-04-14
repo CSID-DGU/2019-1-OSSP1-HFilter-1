7eb72357e8d057c89af4aa2171e9d2a9e1e6e09e
reserved1 <48912108+reserved1@users.noreply.github.com>
2019-04-14 12:39:07 +0900

    Add files via upload

---
+++ main_withID.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+##Copyright 2019. All rights reserved.
+#OSSP in Dongguk University
+#2019-1-OSSP1-HFilter-1
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import sys
+import num_withID
+
+# Main
+path_men = input("남자파일 경로 입력하세요 : ")
+path_women = input("여자파일 경로 입력하세요 : ")
+path_union = num_withID.merge_file_union(path_men, path_women)
+
+# ID와 함께 구합니다.
+path_men = num_withID.makeLabelByTxt_withID(path_men)
+path_women = num_withID.makeLabelByTxt_withID(path_women)
+path_union = num_withID.makeLabelByTxt_withID(path_union)
+
+# Error
+if path_men == None or path_women == None or path_union == None:
+    sys.exit(1)
+
+select = input("히스토그램을 그립니까?(Yes/No) ")
+
+if select=="예" or select=="Yes" or select=="yes"    or select=="1" or select=="o" or select=="ok":
+    num_withID.makeHistogram_IDkorean(path_men, "페이지", "좋아요")
+    num_withID.makeHistogram_IDkorean(path_women, "페이지", "좋아요")
+    num_withID.makeHistogram_IDkorean(path_union, "페이지", "좋아요")
+
+# End of Main
+
